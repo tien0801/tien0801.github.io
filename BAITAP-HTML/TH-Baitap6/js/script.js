95dffcaf28d0836d0f7274fdf702eaee0d38ff5b
VBT\VBT <vubatientien@gmail.com>
2017-07-26 10:50:32 +0700

    update baitap html css 6

--- BAITAP-HTML/TH-Baitap6/js/script.js
+++ BAITAP-HTML/TH-Baitap6/js/script.js
@@ -1,41 +1,48 @@
-var CHECK1 = true;
-var CHECK2 = true;
-var CHECK3 = true;
+
 /*
     Function check id, then show or hidden content id
 */
+var check = true;
+
 function click1(id) {
+	var div1 = document.getElementById('div1');
+	var icon1 = document.getElementById('icon-control');
+	var div2 = document.getElementById('div2');
+	var icon2 = document.getElementById('icon-control-2');
+	var div3 = document.getElementById('div3');
+	var icon3 = document.getElementById('icon-control-3');
+	
 	if (id == 1) {
-		if (CHECK1) {
-				document.getElementById('div1').style.display = "none"; // hidden content
-				document.getElementById('icon-control').style.background = "url(images/top-movie-trailers-arrows.gif) 0 -20px no-repeat";// change icon on title
+		if (check) {
+				div1.style.display = "none"; // hidden content
+				icon1.style.background = "url(images/top-movie-trailers-arrows.gif) 0 -20px no-repeat";// change icon on title
 		}
 		else {
-				document.getElementById('div1').style.display = "block";// show content
-				document.getElementById('icon-control').style.background = "url(images/top-movie-trailers-arrows.gif) 0 4px no-repeat";// change icon on title
+				div1.style.display = "block";// show content
+				icon1.style.background = "url(images/top-movie-trailers-arrows.gif) 0 4px no-repeat";// change icon on title
 		}
-		CHECK1 = !CHECK1;
+		check = !check;
 	}
 	if (id == 2) {
-		if (CHECK2) {
-				document.getElementById("div2").style.display = "none";
-				document.getElementById('icon-control').style.background = "url(images/top-movie-trailers-arrows.gif) 0 -20px no-repeat";
+		if (check) {
+				div2.style.display = "none";
+				icon2.style.background = "url(images/top-movie-trailers-arrows.gif) 0 -20px no-repeat";
 		}
 		else {
-				document.getElementById("div2").style.display = "block";
-				document.getElementById('icon-control').style.background = "url(images/top-movie-trailers-arrows.gif) 0 4px no-repeat";
+				div2.style.display = "block";
+				icon2.style.background = "url(images/top-movie-trailers-arrows.gif) 0 4px no-repeat";
 		}
-		CHECK2 = !CHECK2;
+		check = !check;
 	}
 	if (id == 3) {
-		if (CHECK3) {
-				document.getElementById("div3").style.display = "none";
-				document.getElementById('icon-control').style.background = "url(images/top-movie-trailers-arrows.gif) 0 -20px no-repeat";
+		if (check) {
+				div3.style.display = "none";
+				icon3.style.background = "url(images/top-movie-trailers-arrows.gif) 0 -20px no-repeat";
 		}
 		else {
-				document.getElementById("div3").style.display = "block";
-				document.getElementById('icon-control').style.background = "url(images/top-movie-trailers-arrows.gif) 0 4px no-repeat";
+				div3.style.display = "block";
+				icon3.style.background = "url(images/top-movie-trailers-arrows.gif) 0 4px no-repeat";
 		}
-		CHECK3 = !CHECK3;
+		check = !check;
 	}
 }
