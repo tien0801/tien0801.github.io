766a8de61f103183d6c22158611e54912429c2ae
tien0801 <vubatientien@gmail.com>
2019-03-01 11:16:26 +0700

    snow-animate-html

---
+++ snow-animate-html/js/yosu-snow.js
@@ -0,0 +1,29 @@
+
+
+/* create the link element */
+var linkElement = document.createElement('link');
+/* add attributes */
+linkElement.setAttribute('rel', 'stylesheet');
+// linkElement.setAttribute('href', 'css/yosu-snow.css');
+linkElement.setAttribute('href', 'http://demo2.laziweb.com/snow-animate-html/css/yosu-snow.css');
+/* attach to the document head */
+document.getElementsByTagName('head')[0].appendChild(linkElement);
+
+var snowFunc = function() {	
+	// var b = document.getElementsByTagName('body');
+
+	var p = document.createElement("div");
+	p.innerHTML =  '   <div class="snow-container">  '  + 
+	 '   		<div class="snow foreground"></div>  '  + 
+	 '   		<div class="snow foreground layered"></div>  '  + 
+	 '   		<div class="snow middleground"></div>  '  + 
+	 '   		<div class="snow middleground layered"></div>  '  + 
+	 '   		<div class="snow background"></div>  '  + 
+	 '   		<div class="snow background layered"></div>  '  + 
+	 '  	</div>  ' ; 
+	document.body.insertBefore(p, document.body.firstChild);
+}	
+
+window.onload = function() {
+	snowFunc();	
+}
