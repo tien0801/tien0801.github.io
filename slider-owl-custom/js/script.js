de386fde8fdb26006b3428d155183eda883ff559
tien0801 <vubatientien@gmail.com>
2020-03-04 14:44:00 +0700

    update owl 2

---
+++ slider-owl-custom/js/script.js
@@ -0,0 +1,135 @@
+$(document).ready(function() {
+       
+    if($('.js-home-slider').length >0) {
+        $('.js-home-slider').owlCarousel({
+            loop: true,
+            // autoplay: true,
+            // autoplayTimeout: 4000,
+            // nestedItemSelector: 'item-gall',
+            margin: 0,
+            responsiveClass:true,
+            items: 3,
+            dots: true,
+            navText : ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
+            rewindNav : true,
+            nav: true,
+            responsive : {
+    		    // breakpoint from 0 up
+    		    0 : {
+    		       items: 1,
+    		    },
+    		    480 : {
+    		        items: 1,
+    		    },
+    		    768 : {
+    		        items: 2,
+    		    },
+    		    1200 : {
+    		        items: 3,
+    		    }
+    		}
+        })
+    }
+
+
+    // setTimeout(function(){
+    //    $('.item-slider iframe').
+
+    // }, 3000);
+    /*var frame = jQuery('.item-slider iframe');
+    var src = frame.attr('src');
+    console.log(src);
+    console.log(frame);
+
+    setTimeout(function(){ 
+        frame.attr('src', src+'?autoplay=1');
+        // $('#ifrm').attr('src', "http://www.google.com")
+    }, 500);*/
+
+    if($('.home-slider-3').length >0) {
+        $('.home-slider-3').owlCarousel({
+            loop: true,
+            // autoplay: true,
+            // autoplayTimeout: 4000,
+            // nestedItemSelector: 'item-gall',
+            margin: 0,
+            responsiveClass:true,
+            items: 1,
+            dots: true,
+            navText : ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
+            rewindNav : true,
+            nav: true,
+        })
+    }
+
+    if($('.js-slider-stage').length >0) {
+        $('.js-slider-stage').owlCarousel({
+            loop: false,
+            // autoplay: true,
+            // autoplayTimeout: 4000,
+            // nestedItemSelector: 'item-gall',
+            margin: 0,
+            responsiveClass:true,
+            items: 3,
+            dots: false,
+            navText : ["",""],
+            rewindNav : true,
+            nav: true,
+            // center: true,
+            stagePadding: 100,
+            responsive : {
+                // breakpoint from 0 up
+                0 : {
+                   items: 1,
+                },
+                480 : {
+                    items: 1,
+                },
+                768 : {
+                    items: 2,
+                },
+                1200 : {
+                    items: 2,
+                }
+            },
+        }).on('changed.owl.carousel', stageSlider);
+    }
+    function stageSlider(slider) {
+        console.log("change slider stage");
+        var currentItem = slider.item.index;
+        console.log(currentItem);
+    }
+
+    if($('.js-slider-center').length >0) {
+        $('.js-slider-center').owlCarousel({
+            loop: false,
+            // autoplay: true,
+            // autoplayTimeout: 4000,
+            // nestedItemSelector: 'item-gall',
+            margin: 0,
+            responsiveClass:true,
+            items: 3,
+            dots: false,
+            navText : ["",""],
+            rewindNav : true,
+            nav: true,
+            center: true,
+            responsive : {
+                // breakpoint from 0 up
+                0 : {
+                   items: 1,
+                },
+                480 : {
+                    items: 1,
+                },
+                768 : {
+                    items: 2,
+                },
+                1200 : {
+                    items: 2,
+                }
+            }
+        });
+    }
+
+});
