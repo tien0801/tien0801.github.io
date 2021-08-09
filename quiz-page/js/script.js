7bce4a119af4bfd0d6d64ac288889ced38bce5d9
tiensynova <70185804+tiensynova@users.noreply.github.com>
2021-08-09 12:54:49 +0700

    update js quiz page

--- quiz-page/js/script.js
+++ quiz-page/js/script.js
@@ -19,10 +19,17 @@ $(document).ready(function() {
         var n_next = $(this).data('next');
         console.log('next:', n_next);
 
-        if(n_next == 0) {
-            $('.vue-quiz').removeClass('active');
-            $('.quiz-result').addClass('active');
-        }
+        // if(n_next == 0) {
+        //     $('.vue-quiz').removeClass('active');
+        //     $('.quiz-result').addClass('active');
+        // }
+
+		var items = $('.vue-quiz .v_question').children(".item").length;
+		if(n_next > items) {
+			console.log('Finish question', items);
+			$('.vue-quiz').removeClass('active');
+        	$('.quiz-result').addClass('active');
+		}
 
 		$(this).find('.check-quiz').prop('checked', true);
 
@@ -67,100 +74,3 @@ $(document).ready(function() {
     });
     
 });
-
-
-
-/*
-* Slider - JQuery
-*
-*/
-$(document).ready(function() {
-	responsiveSlider();
-	console.log(count);
-});
-
-var count = 1;
-
-/*
-* Function Slider
-*
-*/
-function responsiveSlider() {
-	var thumb = $(".thumb_slide");
-	var slider = $("#slider-quiz");
-	var sliderWidth = slider.width();
-	var slideList = $("#sliderWrap");
-	var items = slideList.children("li").length;
-	var prev = $("#prev");
-	var next = $("#next");
-	thumb.first().addClass('active');
-
-	//Select Current Thumb
-	var setThumb = function() {
-		var index = $(".thumb_slide").index(this);
-		index = count - 1;
-		$(".thumb_slide").removeClass('active');
-		$(".thumb_slide").eq(index).addClass('active');
-	}
-
-	//Function Previous slide
-	var prevSlide = function() {
-		if(count > 1) {
-			count = count - 2;
-			slideList.css({"left": ""+ (-count * sliderWidth) + "px"});
-			count ++;
-			setThumb();
-			console.log(count);
-		} else if(count = 1) {
-			count = items - 1;
-			slideList.css({"left": ""+ (-count * sliderWidth) + "px"});
-			count ++;
-			setThumb();
-			console.log(count);
-		}
-	}
-
-	//Function next slide
-	var nextSlide = function() {
-		if(count < items) {
-			slideList.css({"left": "" + (-count * sliderWidth) + "px"});
-			count++;
-			setThumb();
-			console.log(count);
-		} else if(count = items) {
-			slideList.css("left", "0");
-			count = 1;
-			setThumb();
-			console.log(count);
-		}
-	}
-
-	//Click Previous
-	prev.click(function() {
-		prevSlide();
-		clearInterval(myTimer);
-	});
-
-	//Click Next
-	next.click(function() {
-		nextSlide();
-		clearInterval(myTimer);
-	});
-
-	//Auto next slide: 3s
-	var myTimer = setInterval(function() {
-		nextSlide();
-		console.log(count);
-	}, 3000);
-
-	//Set click Thumb
-	$(".thumb_slide").click(function() {
-		clearInterval(myTimer);
-		var index = $(".thumb_slide").index(this);
-		count = index;
-		nextSlide();
-		$(".thumb_slide").removeClass('active');
-		$(".thumb_slide").eq(index).addClass('active');
-		console.log(index);
-	})
-};
